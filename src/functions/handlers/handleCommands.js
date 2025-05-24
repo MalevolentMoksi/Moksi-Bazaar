e75d8175c4373d85e70f3c79f05974df78d5e58f
MalevolentMoksi <enzomorello.pro@gmail.com>
2025-05-24 20:13:01 +0200

    Commands fix(?)

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 module.exports = (client) => {
     client.handleCommands = async (commandFolder) => {
+        const { commands, commandArray } = client;
         const commandFolders = fs.readdirSync(`./src/commands`);
         for (const folder of commandFolders) {
             const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
