12d82150477160649843fd5c99e468b0cefa03a2
MalevolentMoksi <enzomorello.pro@gmail.com>
2025-05-24 20:32:53 +0200

    bot.js fix (i hope)

--- src/bot.js
+++ src/bot.js
@@ -1,25 +1,17 @@
 require('dotenv').config();
-const { token } = process.env;
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
 
-const client = new Client({ intents: GatewayIntentBits.Guilds });
-client.commands = new Collection();
-// client.colour = "";
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands     = new Collection();
 client.commandArray = [];
-client.events        = new Collection();
 
-
+// 1) Load command & event handlers (they immediately register everything)
 require('./functions/handlers/handleCommands')(client);
 require('./functions/handlers/handleEvents')(client);
 
-
-const functionFolders = fs.readdirSync('./src/functions');
-for (const folder of functionFolders) {
-    const functionFiles = fs.readdirSync(`./src/functions/${folder}`).filter(file => file.endsWith('.js'));
-    for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
-}
-
-client.handleEvents();
-client.handleCommands();
-client.login(process.env.TOKEN);
+// 2) Register commands (global + dev guild, if you added that)
+(async () => {
+  await client.handleCommands();
+  // 3) Finally, log in
+  client.login(process.env.TOKEN);
+})();
